867582632e1ce7205a4d5393efc960e98274814f
AaronScallen <aaron.scallen@nisd.net>
2021-01-12 16:04:18 -0600

    updatedFiles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import UserProvider from '../context/userContext'
-
+import '../styles.css'
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
   return (
